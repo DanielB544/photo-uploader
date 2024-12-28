cb7fb3d6a2f046bb19267820e07679072654f8e5
DanielB544 <161108227+DanielB544@users.noreply.github.com>
2024-12-27 18:05:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Client ID and API key from the Developer Console
-const CLIENT_ID = '419971965316-34vqaqd2q4be4r9rb5a8nk2cpenmet0f.apps.googleusercontent.com';
+const CLIENT_ID = '419971965316-51t031aqvk1vhetq7n71ncsl87j4prgc.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyCCPZ6-0rxy8cHo8j631qGcf641qixq9PI';
 
 // Array of API discovery doc URLs for APIs used by the app
