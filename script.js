2c27345b8b61bf3adac7251eac717aeddb1fbf1e
DanielB544 <161108227+DanielB544@users.noreply.github.com>
2024-12-27 18:16:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Client ID and API key from the Developer Console
 const CLIENT_ID = 419971965316;
-const API_KEY = AIzaSyCCPZ6-0;
+const API_KEY = 'AIzaSyCCPZ6-0rxy8cHo8j631qGcf641qixq9PI';
 
 // Array of API discovery doc URLs for APIs used by the app
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
